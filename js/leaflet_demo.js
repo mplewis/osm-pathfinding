c222cc09c184756c8337306ed33663f6b1057cdf
Matthew Lewis <matt@mplewis.com>
2014-03-25 00:37:53 -0500

    This version works with webdis!

--- js/leaflet_demo.js
+++ js/leaflet_demo.js
@@ -2,44 +2,56 @@ var map = L.mapbox.map('map', 'examples.map-9ijuk24y');
 var markers = new L.MarkerClusterGroup();
 
 var locs = {
-  triangle: {
-    desc: 'Triangle Fraternity',
-    coords: [44.983294, -93.237445]
-  }
+  '33308096': 'Triangle Fraternity'
 };
 
-var nodeIcon = L.AwesomeMarkers.icon({
-  icon: 'circle-o',
-  markerColor: 'cadetblue',
-  prefix: 'fa'
+var icons = {
+  node: L.AwesomeMarkers.icon({
+    icon: 'circle-o',
+    markerColor: 'cadetblue',
+    prefix: 'fa'
+  }),
+  favorite: L.AwesomeMarkers.icon({
+    icon: 'star',
+    markerColor: 'blue',
+    prefix: 'fa'
+  })
+};
+
+function getNodeCoords(nodeId) {
+  var redisUrl = 'http://localhost:7379/GET/node:' + nodeId + '?jsonp=nodeCoordData';
+  return $.ajax({
+    type: 'GET',
+    url: redisUrl,
+    jsonpCallback: 'nodeCoordData',
+    contentType: "application/json",
+    dataType: 'jsonp'
+  }).then(function(node) {
+    return node.GET.split(':');
+  });
+}
+
+function getAdjNodes(nodeId) {
+  var redisUrl = 'http://localhost:7379/SMEMBERS/nodeadj:' + nodeId + '?jsonp=adjNodeData';
+  return $.ajax({
+    type: 'GET',
+    url: redisUrl,
+    jsonpCallback: 'adjNodeData',
+    contentType: "application/json",
+    dataType: 'jsonp'
+  }).then(function(node) {
+    return node.SMEMBERS;
+  });
+}
+
+_.pairs(locs).forEach(function(loc) {
+  getNodeCoords(loc[0]).then(function(coords) {
+    console.log(coords);
+    map.setView(coords, 15);
+    L.marker(coords, {icon: icons.favorite}).bindPopup(loc[1]).addTo(map);
+  });
 });
 
-var favoriteIcon = L.AwesomeMarkers.icon({
-  icon: 'star',
-  markerColor: 'blue',
-  prefix: 'fa'
+getAdjNodes('33308096').then(function(nodes) {
+  console.log(nodes);
 });
-
-_.values(locs).forEach(function(loc) {
-  L.marker(loc.coords, {icon: favoriteIcon}).bindPopup(loc.desc).addTo(map);
-});
-
-$.get('data/moa_visited.json').then(function(data) {
-  var timer;
-  timer = setInterval(function() {
-    if (data.length <= 0) {
-      clearInterval(timer);
-    }
-    loc = data.shift();
-    L.circle([loc.lat, loc.lon], 1, {opacity: 0.5}).addTo(map);
-  }, 5);
-});
-
-$.get('data/moa_path.json').then(function(data) {
-  pointList = data.map(function(loc) { return [loc.lat, loc.lon]; });
-  L.polyline(pointList, {color: 'red'}).addTo(map);
-});
-
-map.addLayer(markers);
-
-map.setView(locs.triangle.coords, 15);
